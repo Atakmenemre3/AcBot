e6bd3186970d99f93fdb643cd8a7c9312b9d150f
Atakmenemre3 <64912642+Atakmenemre3@users.noreply.github.com>
2020-08-28 22:21:17 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(` ${client.user.tag}aktif!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'sa') {
+    msg.reply('Aleyküm Selam!');
+  }
+  if (msg.content === '!ar') {
+    msg.reply('https://www.youtube.com/channel/UCbG3kOpo8KhkUfO5E7bYRwA');
+  }
+  if (msg.content === '!ea') {
+    msg.reply('https://www.youtube.com/channel/UCHr4ypysVheou96_qlF9FCw');
+  }
+  if (msg.content === '!yt') {
+    msg.reply('https://www.youtube.com/channel/UCHr4ypysVheou96_qlF9FCw, https://www.youtube.com/channel/UCbG3kOpo8KhkUfO5E7bYRwA');
+  }
+  if (msg.content === '!tag') {
+    msg.reply('Tagımız 《AC》 《AC》 《AC》');
+  }
+
+});
+
+client.login('NzQ4OTAxNDM4NjMzMTQ4NDg3.X0kK0A.g_uu0xVquksRwzT_1aLCC1P-q48');
